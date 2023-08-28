1bd4b6a90ea7a98e96d10aadfa62d2dd7965a406
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-08-28 20:11:50 +0530

    modified:   main_6502.cpp

--- main_6502.cpp
+++ main_6502.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
-using Byte = unsigned char;
+using Byte = unsigned char; 
 using Word = unsigned short;
 
+using u32 = unsigned int;
+using u32 = unsigned short;
 
-struct Mem{
-    
-}
+struct Mem // 6502 Memory
+{
+    static constexpr u32 MAX_MEM = 1024 * 64; // Initialise the memory with 64KB
+    Byte Data[MAX_MEM]; // 64KB of memory
 
-struct CPU
+    void Initialise() // Initialise the memory with zero
+    {
+        for (u32 i = 0; i < MAX_MEM; i++)
+        {
+            Data[i] = 0;
+        }
+    }
+};
+ 
+struct CPU // 6502 CPU
 {
 
     Word PC; // Program Counter
@@ -27,18 +39,27 @@ struct CPU
     Byte V : 1; // Overflow Flag
     Byte N : 1; // Negative Flag
 
-    void Reset(){
-        PC = 0xFFFC;
-        SP = 0x0100;
-        D = 0;
-        A = X = Y = 0;
-        C = Z = I = D = B = V = N = 0;
+    void Reset(Mem &memory)
+    {
+        PC = 0xFFFC; // Program Counter initialised With zero
+        SP = 0x0100; // Stack Pointer initialised With zero
+        D = 0; // Decimal Mode initialised With zero
+        A = X = Y = 0; // All Registers initialised With zero
+        C = Z = I = D = B = V = N = 0; // All Flags initialised With zero
+        memory.Initialise();
+    }
+
+    void Execute(u32 Ticks, Mem &memory) // Execute the instructions in the memory
+    {
+
     }
 };
 
 int main()
 {
-    CPU cpu;
-    cpu.Reset();
+    Mem mem; // Initialise the memory
+    CPU cpu; // Initialise the CPU
+    cpu.Reset(mem); // Reset the CPU
+    cpu.Execute(2, mem); // Execute the instructions in the memory
     return 0;
 }
