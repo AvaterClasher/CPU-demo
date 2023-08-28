02cf0192b3028e4d144543b0a89c7306b43c5df3
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-08-28 21:27:09 +0530

    modified:   main_6502.cpp

--- main_6502.cpp
+++ main_6502.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-using Byte = unsigned char; 
+using Byte = unsigned char;
 using Word = unsigned short;
 
 using u32 = unsigned int;
@@ -12,7 +12,7 @@ using u32 = unsigned short;
 struct Mem // 6502 Memory
 {
     static constexpr u32 MAX_MEM = 1024 * 64; // Initialise the memory with 64KB
-    Byte Data[MAX_MEM]; // 64KB of memory
+    Byte Data[MAX_MEM];                       // 64KB of memory
 
     void Initialise() // Initialise the memory with zero
     {
@@ -21,8 +21,14 @@ struct Mem // 6502 Memory
             Data[i] = 0;
         }
     }
+
+    // Read 1 Byte
+    
+    Byte operator[](u32 Address) const {
+        return Data[Address];
+    }
 };
- 
+
 struct CPU // 6502 CPU
 {
 
@@ -39,27 +45,38 @@ struct CPU // 6502 CPU
     Byte V : 1; // Overflow Flag
     Byte N : 1; // Negative Flag
 
-    void Reset(Mem &memory)
+    void Reset(Mem& memory)
     {
-        PC = 0xFFFC; // Program Counter initialised With zero
-        SP = 0x0100; // Stack Pointer initialised With zero
-        D = 0; // Decimal Mode initialised With zero
-        A = X = Y = 0; // All Registers initialised With zero
+        PC = 0xFFFC;                   // Program Counter initialised With zero
+        SP = 0x0100;                   // Stack Pointer initialised With zero
+        D = 0;                         // Decimal Mode initialised With zero
+        A = X = Y = 0;                 // All Registers initialised With zero
         C = Z = I = D = B = V = N = 0; // All Flags initialised With zero
         memory.Initialise();
     }
 
-    void Execute(u32 Ticks, Mem &memory) // Execute the instructions in the memory
+    Byte FetchByte(u32& Cycles, Mem& memory) // Fetch the instructions from the memory
     {
+        Byte Data = memory[PC];
+        PC++;
+        Cycles--;
+        return Data;
+    }
 
+    void Execute(u32 Cycles, Mem& memory) // Execute the instructions in the memory
+    {
+        while (Cycles > 0)
+        {
+            Byte Ins = FetchByte(Cycles, memory);
+        }
     }
 };
 
 int main()
 {
-    Mem mem; // Initialise the memory
-    CPU cpu; // Initialise the CPU
-    cpu.Reset(mem); // Reset the CPU
+    Mem mem;             // Initialise the memory
+    CPU cpu;             // Initialise the CPU
+    cpu.Reset(mem);      // Reset the CPU
     cpu.Execute(2, mem); // Execute the instructions in the memory
     return 0;
 }
